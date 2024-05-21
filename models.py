c18f7c6e4bfea357cd3fc75bf2dbb4fced9fc704
zdemirgithub <zehrademir.gm@gmail.com>
2024-05-21 20:40:52 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,17 @@
+from app import mongo
+
+# MongoDB models
+# Example:
+# class Book(Document):
+#     title = StringField()
+#     author = StringField()
+#     description = StringField()
+#     rating = FloatField()
+#     # Add more fields as needed
+
+# class Review(Document):
+#     book_id = ObjectIdField()
+#     username = StringField()
+#     rating = FloatField()
+#     comment = StringField()
+#     # Add more fields as needed
